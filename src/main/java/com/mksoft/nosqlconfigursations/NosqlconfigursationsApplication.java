12bc482d435cbeb566a6f13ee6b4dea33f053178
Manjeet Kumar <manjeet260190@gmail.com>
2025-06-17 02:21:59 +0530

    Changes in main for the Mono parallel service call

--- src/main/java/com/mksoft/nosqlconfigursations/NosqlconfigursationsApplication.java
+++ src/main/java/com/mksoft/nosqlconfigursations/NosqlconfigursationsApplication.java
@@ -5,6 +5,10 @@ import com.mksoft.nosqlconfigursations.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class NosqlconfigursationsApplication {
@@ -16,6 +20,23 @@ public class NosqlconfigursationsApplication {
 
 		// User user = User.builder().userId("123456").userEmail("manjeet@gmail.com").userName("Manjeet").build();
 		//userService.saveUser(user);
+
+		Mono<Map<Object,Object>> task1 = processTask("Task1");
+		Mono<Map<Object,Object>> task2 = processTask("Task2");
+		Mono<Map<Object,Object>> task3 = processTask("Task3");
+		Mono<Map<Object,Object>> task4 = processTask("Task4");
+		Mono<Map<Object,Object>> task5 = processTask("Task5");
+		Mono<Map<Object,Object>> task6 = processTask("Task6");
+
+		Mono.zip(task1, task2,task3,task4,task5,task6)
+				.map(tuple -> tuple.getT1() + " & " + tuple.getT2()+ " & " + tuple.getT3()+ " & " + tuple.getT4()+ " & " + tuple.getT5()+ " & " + tuple.getT6())
+				.subscribe(System.out::println);
+	}
+
+	private static  Mono<Map<Object,Object>> processTask(String task) {
+		HashMap<Object,Object> map = new HashMap<>();
+		map.put("Key",task);
+		return Mono.just(map);
 	}
 
 }
